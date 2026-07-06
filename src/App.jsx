f9ebb885f21ff3f47f24f54ae3cc85423adb0d1d
HISSANE Omar Adam <hissaneomaradam@gmail.com>
2026-07-06 04:38:11 +0100

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ColorCardRandom from "./components/ColorCardRandom";
 import Pick from "./components/Pick";
 import Results from "./components/Results";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Analytics />
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<ColorCardRandom />} />
         <Route path="/pick" element={<Pick />} />
